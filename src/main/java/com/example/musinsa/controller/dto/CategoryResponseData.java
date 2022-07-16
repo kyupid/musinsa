946ec5da2f2eca099f2e4fbb10d9e6b188ee26d2
kyupid <iamscvv@gmail.com>
2022-07-16 21:57:45 +0900

    add: 카테고리 목록 호출 DTO 추가

---
+++ src/main/java/com/example/musinsa/controller/dto/CategoryResponseData.java
@@ -0,0 +1,11 @@
+package com.example.musinsa.controller.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CategoryResponseData {
+    private Integer id;
+    private String branch;
+    private Integer level;
+    private String name;
+}
--- src/main/java/com/example/musinsa/controller/dto/CategoryResponseDto.java
+++ src/main/java/com/example/musinsa/controller/dto/CategoryResponseDto.java
@@ -1,4 +1,10 @@
 package com.example.musinsa.controller.dto;
 
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
 public class CategoryResponseDto {
+    private List<CategoryResponseData> categories;
 }
