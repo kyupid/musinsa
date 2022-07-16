b9686803b885ff89d61d9459bd13674d8d233ac0
kyupid <iamscvv@gmail.com>
2022-07-16 11:44:57 +0900

    add: 마이바티스 결과 rows와 매핑할 객체 생성

---
+++ src/main/java/com/example/vo/Category.java
@@ -0,0 +1,8 @@
+package com.example.vo;
+
+public class Category {
+    private Integer id;
+    private String name;
+    private String code;
+    private Integer level;
+}
---
+++ src/main/java/com/example/vo/LastInsertCateCode.java
@@ -0,0 +1,7 @@
+package com.example.vo;
+
+public class LastInsertCateCode {
+    private Integer rootId;
+    private Integer level;
+    private String last_insert_code;
+}
