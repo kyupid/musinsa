0dd14815a0ae874bf31f2d981daaa5ed146e6449
kyupid <iamscvv@gmail.com>
2022-07-16 15:51:59 +0900

    add: 카테고리 컬럼 `branch` 추가
    - last_insert_cate_code 테이블 대체

--- src/main/java/com/example/musinsa/vo/Category.java
+++ src/main/java/com/example/musinsa/vo/Category.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public class Category {
     private Integer id;
+    private Integer branch;
     private String name;
     private String code;
     private Integer level;
--- src/main/resources/h2/schema.sql
+++ src/main/resources/h2/schema.sql
@@ -1,7 +1,7 @@
 create table category
 (
     id int auto_increment,
-    branch
+    branch int not null,
     name varchar(100) not null,
     code varchar(300) not null,
     level int not null,
