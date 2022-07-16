ab3cee66ed82a097c1ca2bfde458d2b8ddec18f4
kyupid <iamscvv@gmail.com>
2022-07-16 15:49:29 +0900

    delete: last_insert_cate_code 테이블, 매핑 객체 삭제
    - 마지막에 넣은 code를 불러오기 위해서 따로 테이블을 만든건데 기존 테이블에서 간단히 select해도 해당 문제를 해결할 수 있다고 생각해서 삭제

--- src/main/java/com/example/musinsa/vo/LastInsertCateCode.java
+++
@@ -1,7 +0,0 @@
-package com.example.musinsa.vo;
-
-public class LastInsertCateCode {
-    private Integer rootId;
-    private Integer level;
-    private String last_insert_code;
-}
--- src/main/resources/h2/schema.sql
+++ src/main/resources/h2/schema.sql
@@ -1,20 +1,12 @@
 create table category
 (
     id int auto_increment,
+    branch
     name varchar(100) not null,
     code varchar(300) not null,
     level int not null,
     constraint category_pk
         primary key (id)
-);
-
-create table last_insert_cate_code
-(
-    root_cate_id int auto_increment,
-    level int not null,
-    last_insert_code varchar(300) not null,
-    constraint last_insert_cate_code_pk
-        primary key (root_cate_id, level)
 );
 
 
