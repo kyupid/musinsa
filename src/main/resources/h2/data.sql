bb8f14f9018b69cdcb4b374103ea3317136f4a27
kyupid <iamscvv@gmail.com>
2022-07-17 14:41:48 +0900

    add: data.sql 개발용 샘플 데이터 추가

---
+++ src/main/resources/h2/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO category (id, branch, code, name, level)
+VALUES (1, 'A', 'A', '카테고리_1', 0);
+INSERT INTO category (id, branch, code, name, level)
+VALUES (2, 'B', 'B', '카테고리_2', 0);
+INSERT INTO category (id, branch, code, name, level)
+VALUES (3, 'C', 'C', '카테고리_3', 0);
+INSERT INTO category (id, branch, code, name, level)
+VALUES (4, 'D', 'D', '카테고리_4', 0);
+INSERT INTO category (id, branch, code, name, level)
+VALUES (5, 'E', 'E', '카테고리_5', 0);
+INSERT INTO category (id, branch, code, name, level)
+VALUES (6, 'A', 'A|A', '카테고리_1_1', 1);
+INSERT INTO category (id, branch, code, name, level)
+VALUES (7, 'A', 'A|B', '카테고리_1_2', 1);
+INSERT INTO category (id, branch, code, name, level)
+VALUES (8, 'A', 'A|A|A', '카테고리_1_1_1', 2);
+INSERT INTO category (id, branch, code, name, level)
+VALUES (9, 'A', 'A|A|A|A', '카테고리_1_1_1_1', 3);
+INSERT INTO category (id, branch, code, name, level)
+VALUES (10, 'A', 'A|A|A|B', '카테고리_1_1_1_2', 3);
