2b3555719352440a80ce25306e68439f7e20690d
kyupid <iamscvv@gmail.com>
2022-07-17 16:34:51 +0900

    add: CRUD API HTTP 응답 상태코드 추가

--- src/main/java/com/example/musinsa/controller/CategoryApiController.java
+++ src/main/java/com/example/musinsa/controller/CategoryApiController.java
@@ -5,6 +5,7 @@ import com.example.musinsa.controller.dto.CategoryEditNameRequestDto;
 import com.example.musinsa.controller.dto.CategoryResponseDto;
 import com.example.musinsa.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,6 +18,7 @@ public class CategoryApiController {
     private final CategoryService categoryService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Integer createCategory(@RequestBody @Valid CategoryCreateRequestDto request) {
         return categoryService.createCategory(request);
     }
@@ -32,11 +34,13 @@ public class CategoryApiController {
     }
 
     @DeleteMapping("/{selectedId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCategory(@PathVariable Integer selectedId) {
         categoryService.deleteCategories(selectedId);
     }
 
     @PatchMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void editCategoryName(@RequestBody @Valid CategoryEditNameRequestDto request) {
         categoryService.editCategoryName(request);
     }
