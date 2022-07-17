426148d21ece59a2c5fc7c5fe65380a285a04e76
kyupid <iamscvv@gmail.com>
2022-07-17 09:32:47 +0900

    test: auto_increment 초기화 안되어 `@Sql` 추가
    - @Transactional은 각 테스트별로 rollback만 시켜주고 auto_increment는 초기화를 안시켜준다.

---
+++ src/main/resources/h2/table-init.sql
@@ -0,0 +1,1 @@
+ALTER TABLE category AUTO_INCREMENT = 1;
--- src/test/java/com/example/musinsa/service/CategoryServiceTest.java
+++ src/test/java/com/example/musinsa/service/CategoryServiceTest.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Sql("classpath:h2/table-init.sql")
 class CategoryServiceTest {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
